5d5034cccbffb6a93e8adee95d21dec37415a81d
Masashi Takahashi <emperor-np@i.softbank.jp>
2017-06-28 14:25:48 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using MyMatching
 using Base.Test
 
-const _deferred_acceptance = my_deferred_acceptance
+const _deferred_acceptance = matching
 const test_matrix = false
 
 function mat2vecs{T<:Integer}(prefs::Matrix{T})
